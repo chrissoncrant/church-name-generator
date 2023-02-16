bd914011813b8c4dc3ab435ad5569187982d88bb
Christopher Soncrant <christopher.soncrant@gmail.com>
2023-02-16 12:21:42 -0800

    initial commit; basic stucture set up

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+//TODO: SETUP OPEN AI STUFF
+
+const PORT = 5035;
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send({message: "Hello there!"});
+})
+
+app.post('/', (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.status(200).send({
+        message: 'Heree\'s the data we receeived: ',
+        data: data
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+})
+
